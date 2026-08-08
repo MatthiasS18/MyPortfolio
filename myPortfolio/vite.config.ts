4d398f64e183fcc485b3cb22518be4355418d6f6
Matthias Saltan <58810@etu.he2b.be>
2026-08-08 16:44:01 +0200

    update

--- myPortfolio/vite.config.ts
+++ myPortfolio/vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
     vueDevTools(),
     tailwindcss(),
   ],
+  base: '/MyPortfolio/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
